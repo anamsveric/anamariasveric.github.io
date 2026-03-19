04fe37f3c0c2d4f226bc5b37cd4bea0256e3b9bc
anamsveric <anam.sveric@gmail.com>
2026-03-19 19:24:26 +0100

    update deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "Building..."
+npm run build
+
+echo "Deploying to gh-pages..."
+git worktree add ../gh-pages-deploy gh-pages
+cp -r dist/. ../gh-pages-deploy/
+cd ../gh-pages-deploy
+git add -A
+git commit -m "Deploy $(date '+%Y-%m-%d %H:%M')"
+git push origin gh-pages
+cd c:/web/anamsveric.github.io
+git worktree prune
+
+echo "Deploy done!"
